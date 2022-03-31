a2f45ec67eb77eb04b846b33486d3685d6ef545c
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-03-31 17:16:19 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+function login() {
+  const emailLogin = document.querySelector('#email-login');
+  const passwordLogin = document.querySelector('#password-login');
+  const buttonLogin = document.querySelector('#button-login');
+
+  buttonLogin.addEventListener('click', () => {
+    if (emailLogin.value === 'tryber@teste.com' && passwordLogin.value === '123456') {
+      alert('Olá, Tryber!');
+    } else if (emailLogin.value !== 'tryber@teste.com' && passwordLogin.value !== '123456') {
+      alert('Email ou senha inválidos.');
+    }
+  });
+}
+
+login();
